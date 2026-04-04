82a807956266b4621a3e59627eb2ea155a20521e
dincia <cipriandincia@gmail.com>
2026-04-04 09:40:50 +0800

    API: Implementación de tiempos de degradación de residuos

--- api.py
+++ api.py
@@ -6,17 +6,25 @@ import torch
 import gradio as gr
 from PIL import Image
 import torchvision.transforms as transforms
-
+import os
 import json
 
-# Definimos las clases 
+# tiempo en descomponerse
+datos_ambientales = {
+    "battery": {"nombre": "Batería / Pila", "tiempo": "500 a 1,000 años"},
+    "biological": {"nombre": "Desechos Biológicos", "tiempo": "Semanas a meses"},
+    "cardboard": {"nombre": "Cartón", "tiempo": "1 año"},
+    "glass": {"nombre": "Vidrio", "tiempo": "4,000 años o más"},
+    "metal": {"nombre": "Metal", "tiempo": "10 a 100 años"},
+    "paper": {"nombre": "Papel", "tiempo": "2 a 5 meses"},
+    "plastic": {"nombre": "Plástico", "tiempo": "100 a 1,000 años"}
+}
+#clases
 clases = ["battery", "biological", "cardboard", "glass", "metal", "paper", "plastic"]
 num_clases = len(clases)
 
 
-import os
-
-# Buscamos la carpeta contenedores de forma automática
+#  la carpeta contenedores de forma automática
 base_path = os.path.join(os.path.dirname(__file__), "contenedores")
 
 contenedores = {
@@ -44,7 +52,7 @@ model.eval()
 
 # Transformaciones
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((128, 128)),
     
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
@@ -65,8 +73,18 @@ def predict(img):
     pred_class = clases[pred_idx]
     confianza = probs[pred_idx].item() * 100
 
-    texto = f"Clasificación: {pred_class.upper()}\n Confianza: {confianza:.2f}%\n Depositar en su contenedor"
+  
+    info = datos_ambientales.get(pred_class, {"nombre": pred_class, "tiempo": "No disponible"})
+
+    texto = (f"MATERIAL: {info['nombre'].upper()}\n"
+             f"CONFIANZA: {confianza:.2f}%\n"
+             f"TIEMPO DE DEGRADACIÓN: {info['tiempo']}\n"
+             f"\n"
+             f"Depositar en su contenedor correspondiente.")
+
+   
     contenedor_path = contenedores.get(pred_class, None)
+    
     contenedor_img = Image.open(contenedor_path) if contenedor_path else None
 
     return texto, contenedor_img
